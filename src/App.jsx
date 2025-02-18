a0fa101bcc0f4ebc73f0ec6dacb68d8d7d89a788
harshit gulati <harshitgulati786@gmail.com>
2025-02-18 20:15:16 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <h1 className='text-red-500  text-4xl'>hahahaha</h1>
+    </div>
+  )
+}
+
+export default App
